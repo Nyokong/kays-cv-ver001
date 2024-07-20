6f7e7852134059ade2a0a2588f23080fa55d71e9
Nyokong <50559078+Nyokong@users.noreply.github.com>
2024-07-20 12:56:42 +0200

    some more designs

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default function experience() {
-  return (
-    <div className='grid grid-cols-3 gap-4 p-5 h-[250px] md:max-w-[1500px]'>
-        <div className='bg-gray-200'>
-            Experience
+    return (
+        <div className='grid grid-cols-3 gap-4 p-5 h-[250px] md:max-w-[1000px]'>
+            <div className='bg-gray-200 col-span-3'>
+                Experience
+            </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function hero() {
     return (
-        <div className="grid grid-cols-3 gap-8 p-5 md:max-w-[1500px]">
+        <div className="grid grid-cols-3 gap-8 p-5 md:max-w-[1000px]">
             <div className=''>
                 <Image
                     src="/corp.png"
@@ -13,8 +13,40 @@ export default function hero() {
                     alt="Profile Image - Author"
                 />
             </div>
+            
             <div className='bg-gray-200 col-span-2'>
+                <div className="flex-auto w-full">
+                    <div className="bg-black p-7 w-full">
+                        <h1 className="relative w-full flex-none mb-2 text-2xl font-semibold text-white">
+                            Khotso Nyokong
+                        </h1>
+                        <div className="relative uppercase text-teal-400">
+                            Junior Software Engineer
+                        </div>
+                    </div>
 
+                    <div className="flex items-baseline my-6">
+                        <div className="space-x-3 px-5 flex text-sm font-medium">
+                        <p>
+                            I am a passionate and dedicated junior software engineer with a strong foundation in software development principles and practices. Proficient in languages such as JavaScript, Python, and Java, I have hands-on experience with frameworks like React, Node.js, and Django. My recent projects include developing web applications and contributing to open-source projects, showcasing my ability to learn quickly and adapt to new technologies.
+                        </p>
+                        </div>
+                    </div>
+
+                    <div className="flex space-x-2 text-sm font-medium">
+                        <div className="flex space-x-4 px-6 py-4">
+                            <button className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-teal-400 text-black" type="submit">
+                                Email
+                            </button>
+                            <button className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-teal-400 text-slate-900" type="button">
+                                download my cv
+                            </button>
+                            <button className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-blue-400 text-white" type="submit">
+                                Linked In
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
